5f0cc20a37f9442c9c40c4c03311e9ad3247085b
owenlee-dev <70532700+owenlee-dev@users.noreply.github.com>
2023-04-24 07:18:56 -0700

    updates

--- DensityDial.js
+++ DensityDial.js
@@ -2,25 +2,15 @@
 // Figure out conversion
 // grid lines
 
-// Density 2000= 10 trees/plot = 2.23m spacing --> 2.1em
-// Density 1800= 9 trees/plot = 2.36m spacing --> 2.5em
-// Density 1600= 8 trees/plot = 2.5m spacing --> 2.8em
-// Density 1400= 7 trees/plot = 2.7m spacing --> 3em
-// Density 1200= 6 trees/plot = 2.9m spacing --> 3.8em
-// Density 1000= 5 trees/plot = 3.16m spacing -->
+// Density 2000= 10 trees/plot = 2.23m spacing 
+// Density 1800= 9 trees/plot = 2.36m spacing 
+// Density 1600= 8 trees/plot = 2.5m spacing 
+// Density 1400= 7 trees/plot = 2.7m spacing 
+// Density 1200= 6 trees/plot = 2.9m spacing 
+// Density 1000= 5 trees/plot = 3.16m spacing 
 // Density 800= 4 trees/plot = 3.8m spacing
 
-let lineInterpolation = (emValue) => {
-  return (
-    -1.14608 * (emValue ^ 6) +
-    32.6147 * (emValue ^ 5) -
-    382.262 * (emValue ^ 4) +
-    2361.3 * (emValue ^ 3) -
-    8107.34 * (emValue ^ 2) +
-    14672.3 * emValue -
-    10936.4
-  ).toFixed(2);
-};
+const meter=50;
 
 const treeSpacing = document.querySelector(".tree-spacing");
 const treeSlider = document.querySelector(".tree-range-slider");
@@ -33,15 +23,20 @@ const gridSpacing = document.querySelector(".grid-spacing");
 
 const treeGrid = document.querySelector(".grid-container");
 
+//initialize spcaing
+treeGrid.style.columnGap = 2.7*meter+"px";
+treeGrid.style.rowGap = 2.7*meter+"px";
+
+
+// tree spacing
 treeSlider.addEventListener("input", (event) => {
-  treeGrid.style.rowGap = event.target.value + "em";
+  treeGrid.style.rowGap = event.target.value*meter + "px";
+  treeSpacing.innerHTML=event.target.value;
 });
 
+// line spacing
 lineSlider.addEventListener("input", (event) => {
-  treeGrid.style.columnGap = event.target.value + "em";
-});
+  treeGrid.style.columnGap = event.target.value*meter + "px";
+  lineSpacing.innerHTML=event.target.value;
 
-gridSlider.addEventListener("input", (event) => {
-  treeGrid.style.columnGap = event.target.value + "em";
-  treeGrid.style.rowGap = event.target.value + "em";
 });
